c56e19ad19983818b7852317023b732089ffbc98
chenhao <chenhaomails@gmail.com>
2024-02-15 17:47:48 +0800

    update create_pods.py, add arg for num_of_gpu

--- create_pods.py
+++ create_pods.py
@@ -6,8 +6,8 @@ import sys
 BENCHMARK = "medtasks"
 MODEL = sys.argv[1] #"mlabonne/NeuralMarcoro14-7B" # @param {type:"string"}
 GPU = sys.argv[2] #"NVIDIA GeForce RTX 3090" # @param ["NVIDIA A100 80GB PCIe", "NVIDIA A100-SXM4-80GB", "NVIDIA A30", "NVIDIA A40", "NVIDIA GeForce RTX 3070", "NVIDIA GeForce RTX 3080", "NVIDIA GeForce RTX 3080 Ti", "NVIDIA GeForce RTX 3090", "NVIDIA GeForce RTX 3090 Ti", "NVIDIA GeForce RTX 4070 Ti", "NVIDIA GeForce RTX 4080", "NVIDIA GeForce RTX 4090", "NVIDIA H100 80GB HBM3", "NVIDIA H100 PCIe", "NVIDIA L4", "NVIDIA L40", "NVIDIA RTX 4000 Ada Generation", "NVIDIA RTX 4000 SFF Ada Generation", "NVIDIA RTX 5000 Ada Generation", "NVIDIA RTX 6000 Ada Generation", "NVIDIA RTX A2000", "NVIDIA RTX A4000", "NVIDIA RTX A4500", "NVIDIA RTX A5000", "NVIDIA RTX A6000", "Tesla V100-FHHL-16GB", "Tesla V100-PCIE-16GB", "Tesla V100-SXM2-16GB", "Tesla V100-SXM2-32GB"]
-NUMBER_OF_GPUS = 1 # @param {type:"slider", min:1, max:8, step:1}
-CONTAINER_DISK = 100 # @param {type:"slider", min:50, max:500, step:25}
+NUMBER_OF_GPUS = sys.argv[3] # @param {type:"slider", min:1, max:8, step:1}
+CONTAINER_DISK = 200 # @param {type:"slider", min:50, max:500, step:25}
 VOLUME_IN_GB = 0
 CLOUD_TYPE = "COMMUNITY" # @param ["COMMUNITY", "SECURE"]
 REPO = "https://github.com/chenhaodev/llm-autoeval.git" # @param {type:"string"}
@@ -15,9 +15,9 @@ TRUST_REMOTE_CODE = False # @param {type:"boolean"}
 DEBUG = False # @param {type:"boolean"}
 
 # @markdown ---
-RUNPOD_TOKEN = sys.argv[3] #"runpod" # @param {type:"string"}
-GITHUB_TOKEN = sys.argv[4] #"github" # @param {type:"string"}
-HF_TOKEN = sys.argv[5]
+RUNPOD_TOKEN = sys.argv[4] #"runpod" # @param {type:"string"}
+GITHUB_TOKEN = sys.argv[5] #"github" # @param {type:"string"}
+HF_TOKEN = sys.argv[6]
 
 # Environment variables
 runpod.api_key = RUNPOD_TOKEN
---
+++ scripts/submit-result/submit-results.py
@@ -0,0 +1,90 @@
+import json
+import logging
+import os
+import argparse
+import time
+
+from llm_autoeval.table import make_table, make_final_table
+from llm_autoeval.upload import upload_to_github_gist
+
+MODEL = 'abacusai/Smaug-72B-v0.1'
+GITHUB_API_TOKEN = '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'
+
+def main(directory: str, elapsed_time: float) -> None:
+    file_path = "result.log"
+    summary = open(file_path, "r").read()
+    upload_to_github_gist(
+        summary, f"{MODEL.split('/')[-1]}-MedTasks.md", GITHUB_API_TOKEN
+    )
+
+    '''
+    # Variables
+    tables = []
+    averages = []
+
+    # Tasks
+    if BENCHMARK == "openllm":
+        tasks = ["ARC", "HellaSwag", "MMLU", "TruthfulQA", "Winogrande", "GSM8K"]
+    elif BENCHMARK == "nous":
+        tasks = ["AGIEval", "GPT4All", "TruthfulQA", "Bigbench"]
+    else:
+        raise NotImplementedError(f"BENCHMARK should be 'openllm' or 'nous' (current value = {BENCHMARK})")
+
+    # Load results
+    for task in tasks:
+        file_path = f"{directory}/{task.lower()}.json"
+        if os.path.exists(file_path):
+            json_data = open(file_path, "r").read()
+            data = json.loads(json_data, strict=False)
+            table, average = make_table(data, task)
+        else:
+            table = ""
+            average = "Error: File does not exist"
+
+        tables.append(table)
+        averages.append(average)
+
+    # Generate tables
+    summary = ""
+    for index, task in enumerate(tasks):
+        summary += f"### {task}\n{tables[index]}\nAverage: {averages[index]}%\n\n"
+    result_dict = {k: v for k, v in zip(tasks, averages)}
+
+    # Calculate the final average, excluding strings
+    if all(isinstance(e, float) for e in averages):
+        final_average = round(sum(averages) / len(averages), 2)
+        summary += f"Average score: {final_average}%"
+        result_dict.update({"Average": final_average})
+    else:
+        summary += "Average score: Not available due to errors"
+
+    # Add elapsed time
+    convert = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+    summary += f"\n\nElapsed time: {convert}"
+
+    # Generate final table
+    final_table = make_final_table(result_dict, MODEL)
+    summary = final_table + "\n" + summary
+
+    # Upload to GitHub Gist
+    upload_to_github_gist(
+        summary, f"{MODEL.split('/')[-1]}-{BENCHMARK.capitalize()}.md", GITHUB_API_TOKEN
+    )
+    '''
+
+
+if __name__ == "__main__":
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Summarize results and upload them.")
+    parser.add_argument("directory", type=str, help="The path to the directory with the JSON results")
+    parser.add_argument("elapsed_time", type=float, help="Elapsed time since the start of the evaluation")
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Check if the directory exists
+    if not os.path.isdir(args.directory):
+        raise ValueError(f"The directory {args.directory} does not exist.")
+
+    # Call the main function with the directory argument
+    main(args.directory, args.elapsed_time)

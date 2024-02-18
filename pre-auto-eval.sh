57affe883c56152e178045c7848262a0f90d32e4
chenhao <chenhaomails@gmail.com>
2024-02-19 00:36:00 +0800

    fix bug

--- pre-auto-eval.sh
+++ pre-auto-eval.sh
@@ -13,10 +13,12 @@ fi
 apt update
 apt install -y screen vim git-lfs
 
-echo "lm_eval --model hf --model_args pretrained=/workspace/model,trust_remote_code=$TRUST_REMOTE_CODE,parallelize=True,load_in_4bit=True --tasks ocn,aocnp,medmcqa,pubmedqa,mmlu_clinical_knowledge,mmlu_college_medicine,mmlu_professional_medicine  --device cuda:0  --batch_size auto  --limit 100 | tee result.log; " > run-eval.sh 
+cd /workspace/; mkdir -p cache model; git clone https://github.com/chenhaodev/lm-evaluation-harness; cd lm-evaluation-harness; pip install -e .;
+pip install transformers_stream_generator einops bitsandbyte tiktoken;
+
+echo "lm_eval --model hf --model_args pretrained=/workspace/model,trust_remote_code=True,parallelize=True,load_in_4bit=True --tasks ocn,aocnp,medmcqa,pubmedqa,mmlu_clinical_knowledge,mmlu_college_medicine,mmlu_professional_medicine  --device cuda:0  --batch_size auto  --limit 100 | tee result.log; " > run-eval.sh 
 echo "cd /workspace/; python /workspace/llm-autoeval/upload-result.py . 9999" > run-upload.sh 
 
-cd /workspace/; mkdir -p cache model; git clone https://github.com/chenhaodev/lm-evaluation-harness; cd lm-evaluation-harness; pip install -e .;
 pip install huggingface_hub; huggingface-cli login --token $HF_TOKEN; huggingface-cli download --resume-download $MODEL --local-dir /workspace/model --local-dir-use-symlinks False --cache-dir /workspace/cache;
 
 sleep infinity

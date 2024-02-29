bad80044f77f5c77103a1aafea961e05325f92d4
Mark McQuade <mark@newagetechie.com>
2024-02-29 18:15:28 -0500

    Update table.py

--- llm_autoeval/table.py
+++ llm_autoeval/table.py
@@ -33,8 +33,8 @@ def calculate_average(data, task):
             return data["results"]["arc_challenge"]["acc_norm,none"] * 100
         elif task == "hellaswag":
             return data["results"]["hellaswag"]["acc_norm,none"] * 100
-        elif task == "mmlu":
-            return data["results"]["mmlu"]["acc,none"] * 100    
+        # elif task == "mmlu":
+        #    return data["results"]["mmlu"]["acc,none"] * 100    
         elif task == "truthfulqa":
             value = data["results"]["truthfulqa_mc2"]["acc,none"]
             return 0.0 if math.isnan(value) else value * 100

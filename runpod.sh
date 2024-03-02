4ce77f49cb8a9151252e546fd29e7f50931d037c
Vlad <vlad@arcee.ai>
2024-03-01 16:23:39 -0800

    HF eval mode fix

--- runpod.sh
+++ runpod.sh
@@ -170,6 +170,7 @@ else
     echo "USE_VLLM =" "$USE_VLLM"
     echo "tensor_parallel_size =" "$tensor_parallel_size"
     echo "data_parallel_size =" "$data_parallel_size"
+    echo "PARALELLIZE =" "$PARALELLIZE"
 
     if [ "$USE_VLLM" == "True" ]; then
       pip install --upgrade vllm
@@ -184,7 +185,7 @@ else
           --output_path ./${BENCHMARK}.json
     else
       echo "Running HF eval"
-      python -m lm_eval --verbosity DEBUG --model hf \
+      python main.py --verbosity DEBUG --model hf \
           --model_args pretrained=${MODEL_ID},dtype=auto,trust_remote_code=$TRUST_REMOTE_CODE,parallelize=${PARALELLIZE} \
           --tasks ${BENCHMARK} \
           --num_fewshot ${NUM_FEWSHOT} \
